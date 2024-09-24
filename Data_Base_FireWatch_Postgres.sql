01eba65df8b9f58fbf0bd73160e8b8d92d491aa8
Geovanne Miranda <153963810+geovannemiranda@users.noreply.github.com>
2024-09-24 00:07:09 -0300

    Add files via upload

---
+++ Data_Base_FireWatch_Postgres.sql
@@ -0,0 +1,11 @@
+CREATE TABLE FireWatch (
+	data DATE,
+	Municipio VARCHAR(100),
+	Estado VARCHAR(100),
+	Bioma VARCHAR(100),
+	Numero_dias_sem_chuva VARCHAR(100),
+	Precipitacao VARCHAR(100),
+	Risco_fogo VARCHAR(100),
+	Risco_medio_incendio VARCHAR(100)
+	
+);
